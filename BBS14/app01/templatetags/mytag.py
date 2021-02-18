b4fbbe9d9e6d1174490f5da64a91b8020dae38ad
YuHao-Lu <18219355833@163.com>
2021-02-18 15:41:36 +0800

    BBS项目

---
+++ BBS14/app01/templatetags/mytag.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+# @date 2021/2/16 13:53
+# @author lufa
+from django import template
+from app01 import models
+from django.db.models import Count
+from django.db.models.functions import TruncMonth
+
+register=template.Library()
+
+# 自定义inclusion_tag
+@register.inclusion_tag('left_menu.html')
+def left_menu(username):
+    # 构造侧边栏需要的数据
+    user_obj=models.UserInfo.objects.filter(username=username).first()
+    blog=user_obj.blog
+    # 1.查询当前用户所有的分类及分类下的文章数
+    category_list = models.Category.objects.filter(blog=blog).annotate(count_num=Count('article__pk')).values_list(
+        'name', 'count_num', 'pk')
+    # print(category_list)
+
+    # 2.查询当前用户所有的标签及标签下的文章数
+    tag_list = models.Tag.objects.filter(blog=blog).annotate(count_num=Count('article__pk')).values_list('name',
+                                                                                                         'count_num',
+                                                                                                         'pk')
+    # print(tag_list)
+
+    # 3.按照年月统计所有的文章
+    date_list = models.Article.objects.filter(blog=blog).annotate(month=TruncMonth('create_time')).values(
+        'month').annotate(count_num=Count('pk')).values_list('month', 'count_num')
+    return locals()
+
+
+
+
+
+
+
+
+
